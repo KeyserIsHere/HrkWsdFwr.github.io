30a853a367661d882be1ce23d25138decb5e8244
Stefan Johnson <sj@scrimpycat.io>
2017-02-25 18:43:58 +1100

    Test uniform colour converter

--- HackingGameTests/HubModuleDisplayTests.m
+++ HackingGameTests/HubModuleDisplayTests.m
@@ -113,6 +113,50 @@
     
     CCPixelDataDestroy(Pixels);
     
+    
+    
+    Buffer = HKHubModuleDisplayConvertBuffer(CC_STD_ALLOCATOR, Display, HKHubModuleDisplayBuffer_UniformColourRGB888);
+    Pixels = CCPixelDataStaticCreate(CC_STD_ALLOCATOR, Buffer, CCColourFormatRGB8Uint, 16, 16, 1);
+    
+    
+    Colour = CCPixelDataGetColour(Pixels, 0, 0, 0);
+    
+    Component = CCColourGetComponent(Colour, CCColourFormatChannelRed);
+    XCTAssertEqual(Component.u8, 0, @"Should contain the correct colour");
+    
+    Component = CCColourGetComponent(Colour, CCColourFormatChannelGreen);
+    XCTAssertEqual(Component.u8, 0, @"Should contain the correct colour");
+    
+    Component = CCColourGetComponent(Colour, CCColourFormatChannelBlue);
+    XCTAssertEqual(Component.u8, 0, @"Should contain the correct colour");
+    
+    
+    Colour = CCPixelDataGetColour(Pixels, 1, 0, 0);
+    
+    Component = CCColourGetComponent(Colour, CCColourFormatChannelRed);
+    XCTAssertEqual(Component.u8, (3 * 17) + (2 * 68), @"Should contain the correct colour");
+    
+    Component = CCColourGetComponent(Colour, CCColourFormatChannelGreen);
+    XCTAssertEqual(Component.u8, (3 * 17), @"Should contain the correct colour");
+    
+    Component = CCColourGetComponent(Colour, CCColourFormatChannelBlue);
+    XCTAssertEqual(Component.u8, (3 * 17), @"Should contain the correct colour");
+    
+    
+    Colour = CCPixelDataGetColour(Pixels, 2, 0, 0);
+    
+    Component = CCColourGetComponent(Colour, CCColourFormatChannelRed);
+    XCTAssertEqual(Component.u8, 0, @"Should contain the correct colour");
+    
+    Component = CCColourGetComponent(Colour, CCColourFormatChannelGreen);
+    XCTAssertEqual(Component.u8, 0, @"Should contain the correct colour");
+    
+    Component = CCColourGetComponent(Colour, CCColourFormatChannelBlue);
+    XCTAssertEqual(Component.u8, (3 * 68), @"Should contain the correct colour");
+    
+    
+    CCPixelDataDestroy(Pixels);
+    
     HKHubModuleDestroy(Display);
 }
 
