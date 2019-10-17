018f0f2fda8989d6ba78f904140f9a6739754a50
Stefan Johnson <sj@scrimpycat.io>
2019-10-17 19:47:07 +1100

    Generate presets

--- gen_container_types.sh
+++ gen_container_types.sh
@@ -1,1 +1,1 @@
-ruby deps/Blob2D/deps/CommonGameKit/deps/CommonC/gen_container_types.rb src deps/Blob2D/src deps/Blob2D/deps/CommonGameKit/src deps/Blob2D/deps/CommonGameKit/deps/CommonC/CommonC
+ruby deps/Blob2D/deps/CommonGameKit/deps/CommonC/gen_container_types.rb --preset src deps/Blob2D/src deps/Blob2D/deps/CommonGameKit/src deps/Blob2D/deps/CommonGameKit/deps/CommonC/CommonC
