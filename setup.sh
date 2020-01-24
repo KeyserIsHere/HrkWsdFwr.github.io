860f38fc7e6b6008f7439b3b878bdd51e203f5e5
Stefan Johnson <sj@scrimpycat.io>
2020-01-24 23:17:25 +1100

    Add build setup

---
+++ setup.sh
@@ -0,0 +1,17 @@
+git submodule update
+
+cd deps/Blob2D
+sh setup.sh
+
+cd ../../
+rm -rf build
+ruby deps/Blob2D/deps/CommonGameKit/build.rb --executable \
+--define=CC_QUICK_COMPILE=1 \
+--framework=deps/Blob2D/build/Blob2D \
+--dylib=deps/Blob2D/build/Blob2D/CommonGameKit/CommonGameKit.dylib,deps/Blob2D/build/Blob2D/CommonGameKit \
+--header=deps/Blob2D/deps/CommonGameKit/deps/CommonC \
+--header=deps/Blob2D/deps/CommonGameKit/deps/stdatomic \
+--header=deps/Blob2D/deps/CommonGameKit/deps/threads \
+--header=deps/Blob2D/deps/CommonGameKit/deps/tinycthread/source
+cd build
+ninja
