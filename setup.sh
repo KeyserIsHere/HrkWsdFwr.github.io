3cc1bc93ebf034e90d456e48e1a428664f9bd444
Stefan Johnson <sj@scrimpycat.io>
2020-01-26 18:23:55 +1100

    Handle custom build dir

--- setup.sh
+++ setup.sh
@@ -3,15 +3,17 @@ git submodule update
 cd deps/Blob2D
 sh setup.sh
 
+build=${BUILD_DIR:-build}
+
 cd ../../
-rm -rf build
+rm -rf "$build"
 ruby deps/Blob2D/deps/CommonGameKit/build.rb --executable \
 --define=CC_QUICK_COMPILE=1 \
---framework=deps/Blob2D/build/Blob2D \
---dylib=deps/Blob2D/build/Blob2D/CommonGameKit/CommonGameKit.dylib,deps/Blob2D/build/Blob2D/CommonGameKit \
+--framework="deps/Blob2D/$build/Blob2D" \
+--dylib="deps/Blob2D/$build/Blob2D/CommonGameKit/CommonGameKit.dylib","deps/Blob2D/$build/Blob2D/CommonGameKit" \
 --header=deps/Blob2D/deps/CommonGameKit/deps/CommonC \
 --header=deps/Blob2D/deps/CommonGameKit/deps/stdatomic \
 --header=deps/Blob2D/deps/CommonGameKit/deps/threads \
 --header=deps/Blob2D/deps/CommonGameKit/deps/tinycthread/source
-cd build
+cd "$build"
 ninja
