8f2c49c3e90be61d712ea34274de805db1beb076
Stefan Johnson <sj@scrimpycat.io>
2020-03-13 23:00:10 +1100

    Update container types

--- src/ContainerTypes.h
+++ src/ContainerTypes.h
@@ -5,7 +5,7 @@ CC_CONTAINER_DECLARE(CCArray, CCFontGlyph); \
 CC_CONTAINER_DECLARE(CCArray, CC_CONTAINER(CCLinkedList, HKHubArchExecutionGraphInstruction)); \
 CC_CONTAINER_DECLARE(CCArray, HKHubArchAssemblyASTType); \
 CC_CONTAINER_DECLARE(CCArray, HKHubArchExecutionGraphRange); \
-CC_CONTAINER_DECLARE(CCArray, HKHubArchJITBlock); \
+CC_CONTAINER_DECLARE(CCArray, HKHubArchInstructionState); \
 CC_CONTAINER_DECLARE(CCArray, HKHubArchJITBlockRelativeEntry); \
 CC_CONTAINER_DECLARE(CCArray, uint8_t);
 
@@ -42,7 +42,7 @@ CC_CONTAINER_DECLARE(CCDictionary, CCString, HKHubModuleDisplayBufferConverter);
 CC_CONTAINER_DECLARE(CCDictionary, CCString, uint8_t); \
 CC_CONTAINER_DECLARE(CCDictionary, HKHubArchPortID, HKHubArchPortConnection); \
 CC_CONTAINER_DECLARE(CCDictionary, HKHubModuleWirelessTransceiverPacketSignature, uint8_t); \
-CC_CONTAINER_DECLARE(CCDictionary, uint8_t, HKHubArchJITBlockRelativeEntry); \
+CC_CONTAINER_DECLARE(CCDictionary, uint8_t, HKHubArchJITBlockReferenceEntry); \
 CC_CONTAINER_DECLARE(CCDictionary, uint8_t, HKHubArchProcessorDebugBreakpoint); \
 CC_CONTAINER_DECLARE(CCDictionary, uint8_t, size_t);
 
@@ -50,8 +50,7 @@ CC_CONTAINER_DECLARE(CCDictionary, uint8_t, size_t);
 
 #define CC_CONTAINER_DECLARE_PRESET_CCLinkedList() \
 CC_CONTAINER_DECLARE(CCLinkedList, CCArray); \
-CC_CONTAINER_DECLARE(CCLinkedList, HKHubArchExecutionGraphInstruction); \
-CC_CONTAINER_DECLARE(CCLinkedList, HKHubArchProcessorInstructionGraphNode);
+CC_CONTAINER_DECLARE(CCLinkedList, HKHubArchExecutionGraphInstruction);
 
 #define CC_CONTAINER_DECLARE_PRESET_CCList() \
 CC_CONTAINER_DECLARE(CCList, uint64_t);
