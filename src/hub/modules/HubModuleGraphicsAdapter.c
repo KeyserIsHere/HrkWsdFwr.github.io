fbe372a2354420aede55a3badc2c0ed2c162f962
Stefan Johnson <sj@scrimpycat.io>
2021-07-02 08:11:00 +1000

    Set cursor newline offset

--- src/hub/modules/HubModuleGraphicsAdapter.c
+++ src/hub/modules/HubModuleGraphicsAdapter.c
@@ -337,6 +337,14 @@ void HKHubModuleGraphicsAdapterSetCursorTabOffset(HKHubModule Adapter, uint8_t L
     State->attributes[Layer].cursor.tabOffset = Offset;
 }
 
+void HKHubModuleGraphicsAdapterSetCursorNewlineOffset(HKHubModule Adapter, uint8_t Layer, uint8_t Offset)
+{
+    CCAssertLog(Layer < HK_HUB_MODULE_GRAPHICS_ADAPTER_LAYER_COUNT, "Layer must not exceed layer count");
+    
+    HKHubModuleGraphicsAdapterState *State = Adapter->internal;
+    State->attributes[Layer].cursor.newlineOffset = Offset;
+}
+
 const uint8_t *HKHubModuleGraphicsAdapterGetGlyphBitmap(HKHubModule Adapter, CCChar Character, uint8_t AnimationOffset, uint8_t AnimationFilter, uint8_t *Width, uint8_t *Height, uint8_t *PaletteSize)
 {
     HKHubModuleGraphicsAdapterState *State = Adapter->internal;
