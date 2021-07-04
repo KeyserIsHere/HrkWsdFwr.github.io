ee1e2dc7bf992ae9894d0d91e040dade57b1d901
Stefan Johnson <sj@scrimpycat.io>
2021-07-05 04:19:31 +1000

    Add set palette offset

--- src/hub/modules/HubModuleGraphicsAdapter.c
+++ src/hub/modules/HubModuleGraphicsAdapter.c
@@ -366,6 +366,14 @@ void HKHubModuleGraphicsAdapterSetPalettePage(HKHubModule Adapter, uint8_t Layer
     State->attributes[Layer].palette.page = Page;
 }
 
+void HKHubModuleGraphicsAdapterSetPaletteOffset(HKHubModule Adapter, uint8_t Layer, uint8_t Offset)
+{
+    CCAssertLog(Layer < HK_HUB_MODULE_GRAPHICS_ADAPTER_LAYER_COUNT, "Layer must not exceed layer count");
+    
+    HKHubModuleGraphicsAdapterState *State = Adapter->internal;
+    State->attributes[Layer].palette.offset = Offset;
+}
+
 const uint8_t *HKHubModuleGraphicsAdapterGetGlyphBitmap(HKHubModule Adapter, CCChar Character, uint8_t AnimationOffset, uint8_t AnimationFilter, uint8_t *Width, uint8_t *Height, uint8_t *PaletteSize)
 {
     HKHubModuleGraphicsAdapterState *State = Adapter->internal;
