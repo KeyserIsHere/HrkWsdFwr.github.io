60ad2d0e4bac9727465b42685c27cf2fecd3e656
Stefan Johnson <sj@scrimpycat.io>
2021-07-07 09:27:49 +1000

    Set italic

--- src/hub/modules/HubModuleGraphicsAdapter.c
+++ src/hub/modules/HubModuleGraphicsAdapter.c
@@ -382,6 +382,14 @@ void HKHubModuleGraphicsAdapterSetBold(HKHubModule Adapter, uint8_t Layer, _Bool
     State->attributes[Layer].style.bold = Enable;
 }
 
+void HKHubModuleGraphicsAdapterSetItalic(HKHubModule Adapter, uint8_t Layer, _Bool Enable)
+{
+    CCAssertLog(Layer < HK_HUB_MODULE_GRAPHICS_ADAPTER_LAYER_COUNT, "Layer must not exceed layer count");
+    
+    HKHubModuleGraphicsAdapterState *State = Adapter->internal;
+    State->attributes[Layer].style.italic = Enable;
+}
+
 const uint8_t *HKHubModuleGraphicsAdapterGetGlyphBitmap(HKHubModule Adapter, CCChar Character, uint8_t AnimationOffset, uint8_t AnimationFilter, uint8_t *Width, uint8_t *Height, uint8_t *PaletteSize)
 {
     HKHubModuleGraphicsAdapterState *State = Adapter->internal;
